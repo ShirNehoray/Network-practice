41185c391a7783631137ff08554c60f330a91d94
ShirNehoray <shirneho@post.bgu.ac.il>
2024-09-19 11:04:02 +0300

    Update 2.1.R

--- 2.1.R
+++ 2.1.R
@@ -24,3 +24,19 @@ g1 <- igraph::graph.adjacency(A_d, mode = 'directed')
 par(mar=c(0,0,0,0))
 plot(g1)
 
+A_w <- matrix(c(0,1,1,0,0, # An example input matrix
+                1,0,0,1,1,
+                1,0,0,0,0,
+                0,1,0,0,0,
+                0,1,0,0,0),5,5, byrow=F)
+random_weights <- round(rnorm(10, 10, 4),2) # take weights from a normal distribution.
+A_w[lower.tri(A_w)] <- A_w[lower.tri(A_w)]*random_weights # Fill the lower traiangle
+A_w <- A_w+t(A_w) # This makes the matrix symmetric
+isSymmetric(A_w)
+A_w
+library(igraph)
+g <- igraph::graph.adjacency(A_w, mode = 'undirected', weighted = T)
+E(g)$weight
+par(mar=c(0,0,0,0))
+plot(g, edge.color=E(g)$weight, edge.width = E(g)$weight,  weighted = T, vertex.label = E(g)$weight)
+
